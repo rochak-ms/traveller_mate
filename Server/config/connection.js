034b63d2505359e0f105a40bebef9aaf0b962a12
GINA ZIVKOVIC <GINA.ZIVKOVIC91@GMAIL.COM>
2023-02-09 05:51:24 +1000

    set up server

--- Server/config/connection.js
+++ Server/config/connection.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/travela', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/flightmanager', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: false,
+  useFindAndModify: false
 });
 
 module.exports = mongoose.connection;
--- Server/utils/auth.js
+++ Server/utils/auth.js
@@ -1,38 +1,40 @@
 const jwt = require('jsonwebtoken');
 
-// set token secret and expiration date
-const secret = 'comeooonnnn';
-const expiration = '3h';
+const secret = 'helootraveller';
+const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
   authMiddleware: function ({ req }) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.query.token || req.headers.authorization;
-
-    // ["Bearer", "<tokenvalue>"]
+    let token = req.body.token || req.query.token || req.headers.authorization;
+    
     if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+      token = token
+        .split(' ')
+        .pop()
+        .trim();
     }
 
     if (!token) {
       return req;
     }
 
-    // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
-    } catch {
+    }
+    catch {
       console.log('Invalid token');
-      
     }
 
-   return req;
+    return req;
   },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
+  signToken: function ({ firstName, email, _id }) {
+    const payload = { firstName, email, _id };
 
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+    return jwt.sign(
+      { data: payload },
+      secret,
+      { expiresIn: expiration }
+    );
+  }
 };
