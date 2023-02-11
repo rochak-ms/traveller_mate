90dc9450da4a9a3b2c539fb967118b8236be6e7c
rocky <rochak.ms@gmail.com>
2023-02-11 09:26:25 +0800

    Added serviceWorker function, assets and created needed file and folder for frontend

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "semantic-ui-css/semantic.min.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
+import DayjsUtils from "dayjs";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <MuiPickersUtilsProvider utils={DayjsUtils}>
     <App />
-  </React.StrictMode>
+  </MuiPickersUtilsProvider>,
+  document.querySelector("#root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.register();
